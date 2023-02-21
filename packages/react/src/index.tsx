be1ac40c818975e48fdf3696ebfc530072c7da09
felipebdn <71861370+felipebdn@users.noreply.github.com>
2023-02-21 12:17:38 -0300

    121721022023

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,9 @@
+import { styled } from './styles'
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+})
+
+export function App() {
+  return <Button>Hello World</Button>
+}
